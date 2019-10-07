ec795b49ab289ecae31a78e680195649ed02cbd1
Máté <mate1011@easv365.dk>
2019-10-07 10:51:34 +0200

    Interfaces added

---
+++ RubberDuckShop.Core/ApplicationService/Services/DuckPatternService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RubberDuckShop.Core.ApplicationService.Services
+{
+    public class DuckPatternService
+    {
+    }
+}
