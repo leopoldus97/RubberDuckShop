85a13e5651c0591ccbbaafb85a9ae8955d996e4f
Máté <mate1011@easv365.dk>
2019-10-07 10:22:23 +0200

    Initial commit

---
+++ RubberDuckShop.Core.Entity/RubberDuck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RubberDuckShop.Core.Entity
+{
+
+    public enum DuckSize { 
+        SMALL, MEDIUM, LARGE, XXL
+    }
+
+    public enum DuckGender { 
+        MALE, FEMALE
+    }
+
+    public class RubberDuck
+    {
+        public int ID { get; set; }
+        public DuckColor DuckColor { get; set; }
+        public DuckPattern DuckPattern { get; set; }
+        public DuckSize DuckSize { get; set; }
+        public int Price { get; set; }
+        public DuckGender DuckGender { get; set; }
+        public DuckCostume DuckCostume { get; set; }
+    }
+}
