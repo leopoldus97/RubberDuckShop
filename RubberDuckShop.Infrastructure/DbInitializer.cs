16d0aac2114488567750f6f94abd3e266456c2f9
KLUU69 <a.kevinlau@googlemail.com>
2019-10-08 08:21:36 +0200

    Naming in DbInitializer

--- RubberDuckShop.Infrastructure/DbInitializer.cs
+++ RubberDuckShop.Infrastructure/DbInitializer.cs
@@ -12,80 +12,80 @@ namespace RubberDuckShop.Infrastructure
         {
             ctx.Database.EnsureCreated();
 
-            DuckColor dcol1 = ctx.duckColor.Add(new DuckColor()
+            DuckColor dcol1 = ctx.duckColors.Add(new DuckColor()
             {
                 ID = 1,
                 Name = "Yellow"
             }).Entity;
 
-            DuckColor dcol2 = ctx.duckColor.Add(new DuckColor()
+            DuckColor dcol2 = ctx.duckColors.Add(new DuckColor()
             {
                 ID = 2,
                 Name = "Red"
             }).Entity;
 
-            DuckColor dcol3 = ctx.duckColor.Add(new DuckColor()
+            DuckColor dcol3 = ctx.duckColors.Add(new DuckColor()
             {
                 ID = 3,
                 Name = "Green"
             }).Entity;
 
-            DuckColor dcol4 = ctx.duckColor.Add(new DuckColor()
+            DuckColor dcol4 = ctx.duckColors.Add(new DuckColor()
             {
                 ID = 4,
                 Name = "Black"
             }).Entity;
 
-            DuckColor dcol5 = ctx.duckColor.Add(new DuckColor()
+            DuckColor dcol5 = ctx.duckColors.Add(new DuckColor()
             {
                 ID = 5,
                 Name = "Brown"
             }).Entity;
 
-            DuckColor dcol6 = ctx.duckColor.Add(new DuckColor()
+            DuckColor dcol6 = ctx.duckColors.Add(new DuckColor()
             {
                 ID = 6,
                 Name = "Pink"
             }).Entity;
 
-            DuckPattern dpat1 = ctx.duckPattern.Add(new DuckPattern()
+            DuckPattern dpat1 = ctx.duckPatterns.Add(new DuckPattern()
             {
                 ID = 1,
                 Name = "Spotted"
             }).Entity;
 
-            DuckPattern dpat2 = ctx.duckPattern.Add(new DuckPattern()
+            DuckPattern dpat2 = ctx.duckPatterns.Add(new DuckPattern()
             {
                 ID = 2,
                 Name = "Striped"
             }).Entity;
 
-            DuckPattern dpat3 = ctx.duckPattern.Add(new DuckPattern()
+            DuckPattern dpat3 = ctx.duckPatterns.Add(new DuckPattern()
             {
                 ID = 3,
                 Name = "No Pattern"
             }).Entity;
 
 
-            DuckCostume dcost1 = ctx.duckCostume.Add(new DuckCostume()
+            DuckCostume dcost1 = ctx.duckCostumes.Add(new DuckCostume()
             {
                 ID = 1,
                 Name = "Navy"
             }).Entity;
 
-            DuckCostume dcost2 = ctx.duckCostume.Add(new DuckCostume()
+            DuckCostume dcost2 = ctx.duckCostumes.Add(new DuckCostume()
             {
                 ID = 2,
                 Name = "Thor"
             }).Entity;
 
-            DuckCostume dcost3 = ctx.duckCostume.Add(new DuckCostume()
+            DuckCostume dcost3 = ctx.duckCostumes.Add(new DuckCostume()
             {
                 ID = 3,
                 Name = "Jack Sparrow"
             }).Entity;
 
-            DuckCostume dcost4 = ctx.duckCostume.Add(new DuckCostume()
+            DuckCostume dcost4 = ctx.duckCostumes.Add(new DuckCostume()
             {
                 ID = 4,
                 Name = "Nurse"
@@ -98,9 +98,9 @@ namespace RubberDuckShop.Infrastructure
             {
                 ctx.rubberDucks.Add(new RubberDuck() { 
                     ID = i,
-                    DuckColor = ctx.duckColor.FirstOrDefault(col => col.ID == rnd.Next(0, 5)),
-                    DuckCostume = ctx.duckCostume.FirstOrDefault(cos => cos.ID == rnd.Next(0, 3)),
-                    DuckPattern = ctx.duckPattern.FirstOrDefault(pat => pat.ID == rnd.Next(0, 3)),
+                    DuckColor = ctx.duckColors.FirstOrDefault(col => col.ID == rnd.Next(0, 5)),
+                    DuckCostume = ctx.duckCostumes.FirstOrDefault(cos => cos.ID == rnd.Next(0, 3)),
+                    DuckPattern = ctx.duckPatterns.FirstOrDefault(pat => pat.ID == rnd.Next(0, 3)),
                     DuckSize = (DuckSize) rnd.Next(1, 4),
                     DuckGender = (DuckGender) rnd.Next(1, 2),
                     Price = rnd.Next(600, 1200)
